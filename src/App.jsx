9c709791e641671ad9de9310e3a5e119514a77d1
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2026-03-11 02:27:31 +0200

    Bug Fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,23 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import 'paper-css/paper.min.css';
-// import DatePicker from 'react-datepicker'
 // Date Picker
 import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+// import DatePicker from 'react-datepicker'
 
 function App() {
 
-  const [selectedCountry, setselectedCountry] = useState("")
+  const [selectedCountry, setSelectedCountry] = useState("")
   const [countryData, setCountryData] = useState([])
-  const [selectedYear, setSelectedYear] = useState("")
+  const [selectedYear, setSelectedYear] = useState("2026")
   const [vacationList, setVacationList] = useState([])
-
 
   // API importing data
 
   // Fetching Country codes
-  // get/api/v3/AvailableCountries
   useEffect(() => {
     const fetchContries = async () => {
       try {
@@ -33,76 +31,80 @@ function App() {
     }
 
     fetchContries()
-}, [])
+  }, [])
 
-// Fetching Country Vacation
-// get/api/v3/PublicHolidays/{year}/{countryCode}
-useEffect(() => {
-  const fetchHolidays = async () => {
-    try {
-      // change when using selecting date to `https://date.nager.at/api/v3/PublicHolidays/${selectedYear}/${selectedCountry}
-      const response = await fetch (`https://date.nager.at/api/v3/PublicHolidays/2026/${selectedCountry}`)
-      const data = await response.json()
-      setVacationList(data)
-      console.log(data)
-      // console.log(`Year: ${selectedYear}, Country: ${selectedCountry}`)
-    } catch (err) {
-      console.error(err)
+  // Fetching Country Vacation
+  // get/api/v3/PublicHolidays/{year}/{countryCode}
+  useEffect(() => {
+    if (!selectedCountry) return
+
+    const fetchHolidays = async () => {
+      try {
+        // change when using selecting date to `
+        const response = await fetch(`https://date.nager.at/api/v3/PublicHolidays/${selectedYear}/${selectedCountry}`)
+        const data = await response.json()
+        setVacationList(data)
+        console.log(data)
+      } catch (err) {
+        console.error(err)
+      }
     }
-  }
 
-  fetchHolidays()
-}, [selectedCountry, selectedYear])
+    fetchHolidays()
+  }, [selectedCountry, selectedYear])
 
-  return (  
+  return (
     <>
+      <h1 style={{ width: '80%' }}>Public holidays app</h1>
 
+      {/* Selecting Country */}
+      <select style={{ width: '35%' }} id="options" value={selectedCountry} onChange={(e) => setSelectedCountry(e.target.value)}>
+        <option value={""}>-- Select a Country --</option>
+        {countryData.map((country) => {
+          return (
+            <option key={country.countryCode} value={country.countryCode}>
+              {country.name}
+            </option>
+          )
+        })}
+      </select>
 
-        <h1 style={{width : '80%'}}>Public holidays app</h1>
-        
-        {/* Selecting Country */}
-        <select style={{width : '35%'}} id="options" value={selectedCountry} onChange={(e) => setselectedCountry(e.target.value)}>
-          <option  value={""}>-- Select a Country --</option>
-          {countryData.map((country) => {
-            console.log(selectedCountry)
-            return(<option key={country.countryCode} value={country.countryCode}>{country.name}</option>)
-          })}
-        </select>
-          
-        {/* Selecting Year */}
-          {/* <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DemoContainer components={['DatePicker', 'DatePicker']}>
-              <DatePicker 
-                openTo="year"
-                label={'"year"'} 
-                selected={selectedYear}
-                onChange={(date) => setSelectedYear(date)}
-                showYearPicker
-                dateFormat={"yyyy"}
-                format="YYYY" 
-                />
-            </DemoContainer>
-          </LocalizationProvider> */}
+      {/* Selecting Year */}
 
-          {/* <DatePicker
+      {/* <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DemoContainer components={['DatePicker', 'DatePicker']}>
+          <DatePicker
+            openTo="year"
+            label={'"year"'}
             selected={selectedYear}
-            onChange={(date) => setSelectedYear(date.getFullYear())}
+            onChange={(date) => setSelectedYear(date)}
             showYearPicker
             dateFormat={"yyyy"}
-            className="my-custom-input"
-            
-          /> */}
+            format="YYYY"
+          />
+        </DemoContainer>
+      </LocalizationProvider> */}
 
-          {/* Vacations list */}
-          <div >
-            <ul  className="inline">
-              {vacationList.map((countryVacation) => {
-                return (
-                  <li key={countryVacation.date}><strong>{countryVacation.localName}: {countryVacation.date}<br/></strong></li>
-                )
-              })}
-            </ul>
-          </div>
+      {/* <DatePicker
+        selected={selectedYear}
+        onChange={(date) => setSelectedYear(date.getFullYear())}
+        showYearPicker
+        dateFormat={"yyyy"}
+        className="my-custom-input"
+      /> */}
+
+      {/* Vacations list */}
+      <div>
+        <ul className="inline">
+          {vacationList.map((countryVacation) => {
+            return (
+              <li key={countryVacation.date}>
+                <strong>{countryVacation.localName}: {countryVacation.date}<br /></strong>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
     </>
   )
 }
